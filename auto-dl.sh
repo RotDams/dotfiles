40f3b00cf11cd71d14ad3edbdaacc1c9baba54b8
dams <dchampeyroux.dc@gmail.com>
2020-09-06 17:44:41 +0200

    fix(auto-dl): change theme

--- auto-dl.sh
+++ auto-dl.sh
@@ -47,5 +47,5 @@ sudo ~/.config/misc-config/noto-font.sh
 ln -s ~/.config/misc-config/.Xresources ~/.Xresources   
 
 $PACMAN -S --needed lightdm-webkit2-greeter lightdm-webkit-theme-litarvan
-sudo sed -i 's/greeter-session=/greeter-session=lightdm-greeter-session\n# /g' /etc/lightdm/lightdm.conf
+sudo sed -i 's/greeter-session=/greeter-session=lightdm-webkit2-greeter\n# /g' /etc/lightdm/lightdm.conf
 sudo sed -i 's/webkit_theme        =/webkit_theme        =litarvan# /g' /etc/lightdm/lightdm-webkit2-greeter.conf
