8583d3631e76000e374c29b85b1a631545cd3d38
dams <dchampeyroux.dc@gmail.com>
2020-09-07 15:33:16 +0200

    fix(install.sh): as sudo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
  
  
 username="$(logname)"
-pacman -Syyu --noconfirm
+sudo pacman -Syyu --noconfirm
 
 git clone https://aur.archlinux.org/yay.git /home/$username/temp-conf/yay
 git clone https://github.com/RotDams/dotfile /home/$username/temp-conf/dotfile
