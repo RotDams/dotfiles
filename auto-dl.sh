fd9c4b0316b2495d9c03199973dd346c61b30752
dams <dchampeyroux.dc@gmail.com>
2020-09-07 03:51:04 +0200

    fix(auto-dl): log into variable

--- auto-dl.sh
+++ auto-dl.sh
@@ -2,19 +2,21 @@
 
 PACMAN="sudo pacman --noconfirm"
 YAY="yay -S --noconfirm"
+username=$logname
+echo "\ncurrent user name = $logname\n"
 
 sudo pacman -Syyu --noconfirm
-git clone https://aur.archlinux.org/yay.git /home/logname/temp-conf/yay
-cd /home/logname/temp-conf/yay
-sudo -u logname makepkg -si --noconfirm
+git clone https://aur.archlinux.org/yay.git /home/$username/temp-conf/yay
+cd /home/$username/temp-conf/yay
+sudo -u $username makepkg -si --noconfirm
 
 $PACMAN -S feh
 
 $YAY i3-gaps
 
-git clone https://github.com/RotDams/dotfile /home/logname/temp-conf/dotfile
-mv /home/logname/temp-conf/dotfile/* /home/logname/.config/
-rm -rf /home/logname/temp-conf
+git clone https://github.com/RotDams/dotfile /home/$username/temp-conf/dotfile
+mv /home/$username/temp-conf/dotfile/* /home/$username/.config/
+rm -rf /home/$username/temp-conf
 
 $PACMAN -S blueberry 
 
@@ -23,27 +25,27 @@ $PACMAN -S perl
 $PACMAN -S rofi
 
 $PACMAN -S gvim 
-ln -s /home/logname/.config/misc-config/.vimrc /home/logname/.vimrc
+ln -s /home/$username/.config/misc-config/.vimrc /home/$username/.vimrc
 
 $PACMAN -S zsh
-ln -s /home/logname/.config/misc-config/.zshrc /home/logname/.zshrc
+ln -s /home/$username/.config/misc-config/.zshrc /home/$username/.zshrc
 
 $PACMAN -S tree
 $PACMAN -S code
 $PACMAN -S conky-lua-nv
-ln -s /home/logname/.config/misc-config/.conkyrc /home/logname/.conkyrc
+ln -s /home/$username/.config/misc-config/.conkyrc /home/$username/.conkyrc
 
 $PACMAN -S picom
-mkdir /home/logname/.config/picom/
-cp /etc/xdg/picom.conf /home/logname/.config/picom/picom.conf
-picom --config /home/logname/.config/picom/picom.conf
+mkdir /home/$username/.config/picom/
+cp /etc/xdg/picom.conf /home/$username/.config/picom/picom.conf
+picom --config /home/$username/.config/picom/picom.conf
 
 $YAY polybar
 $YAY ttf-font-awesome-4
 $YAY ttf-joypixels
 $PACMAN -S bluez-utils
-sudo /home/logname/.config/misc-config/noto-font.sh --noconfirm
-ln -s /home/logname/.config/misc-config/.Xresources /home/logname/.Xresources   
+sudo /home/$username/.config/misc-config/noto-font.sh --noconfirm
+ln -s /home/$username/.config/misc-config/.Xresources /home/$username/.Xresources   
 
 $PACMAN -S --needed lightdm-webkit2-greeter lightdm-webkit-theme-litarvan
 sudo sed -i 's/greeter-session=/greeter-session=lightdm-webkit2-greeter\n# /g' /etc/lightdm/lightdm.conf
