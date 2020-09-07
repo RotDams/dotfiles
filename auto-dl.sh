c0e92949e2ec4507e7e7e2fc66f70961f67866fc
dams <dchampeyroux.dc@gmail.com>
2020-09-07 03:20:08 +0200

    feat(auto-dl): logname added

--- auto-dl.sh
+++ auto-dl.sh
@@ -4,17 +4,17 @@ PACMAN="sudo pacman --noconfirm"
 YAY="yay -S --noconfirm"
 
 sudo pacman -Syyu --noconfirm
-git clone https://aur.archlinux.org/yay.git ~/temp-conf/yay
-cd ~/temp-conf/yay
-makepkg -si --noconfirm
+git clone https://aur.archlinux.org/yay.git /home/logname/temp-conf/yay
+cd /home/logname/temp-conf/yay
+sudo -u makepkg -si --noconfirm
 
 $PACMAN -S feh
 
 $YAY i3-gaps
 
-git clone https://github.com/RotDams/dotfile ~/temp-conf/dotfile
-mv ~/temp-conf/dotfile/* ~/.config/
-rm -rf ~/temp-conf
+git clone https://github.com/RotDams/dotfile /home/logname/temp-conf/dotfile
+mv /home/logname/temp-conf/dotfile/* /home/logname/.config/
+rm -rf /home/logname/temp-conf
 
 $PACMAN -S blueberry 
 
@@ -23,27 +23,27 @@ $PACMAN -S perl
 $PACMAN -S rofi
 
 $PACMAN -S gvim 
-ln -s ~/.config/misc-config/.vimrc ~/.vimrc
+ln -s /home/logname/.config/misc-config/.vimrc /home/logname/.vimrc
 
 $PACMAN -S zsh
-ln -s ~/.config/misc-config/.zshrc ~/.zshrc
+ln -s /home/logname/.config/misc-config/.zshrc /home/logname/.zshrc
 
 $PACMAN -S tree
 $PACMAN -S code
 $PACMAN -S conky-lua-nv
-ln -s ~/.config/misc-config/.conkyrc ~/.conkyrc
+ln -s /home/logname/.config/misc-config/.conkyrc /home/logname/.conkyrc
 
 $PACMAN -S picom
-mkdir ~/.config/picom/
-cp /etc/xdg/picom.conf ~/.config/picom/picom.conf
-picom --config ~/.config/picom/picom.conf
+mkdir /home/logname/.config/picom/
+cp /etc/xdg/picom.conf /home/logname/.config/picom/picom.conf
+picom --config /home/logname/.config/picom/picom.conf
 
 $YAY polybar
 $YAY ttf-font-awesome-4
 $YAY ttf-joypixels
 $PACMAN -S bluez-utils
-sudo ~/.config/misc-config/noto-font.sh --noconfirm
-ln -s ~/.config/misc-config/.Xresources ~/.Xresources   
+sudo /home/logname/.config/misc-config/noto-font.sh --noconfirm
+ln -s /home/logname/.config/misc-config/.Xresources /home/logname/.Xresources   
 
 $PACMAN -S --needed lightdm-webkit2-greeter lightdm-webkit-theme-litarvan
 sudo sed -i 's/greeter-session=/greeter-session=lightdm-webkit2-greeter\n# /g' /etc/lightdm/lightdm.conf
