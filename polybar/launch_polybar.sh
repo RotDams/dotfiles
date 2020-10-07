e57be8f467136b85955fea8c6134d932f1d3756a
dams <dchampeyroux.dc@gmail.com>
2020-10-07 11:59:34 +0200

    fix (polybar): polybar temp for every computer

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -4,19 +4,24 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-  # Launch bar1 and bar2
+# Launch bar1 and bar2
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Core 0"  ]; then
+        export HWMON_PATH="$i"
+    fi
+done
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    if [ $m == 'eDP1' ] 
-	  then		
-		  MONITOR=$m polybar --reload default &
-	  elif [ $m == 'HDMI0' ]
-	  then
-		  MONITOR=$m polybar --reload default &
-  	else
-		  MONITOR=$m polybar --reload screen-2 &
-	  fi     
-  done
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        if [ $m == 'eDP1' ] 
+        then		
+            MONITOR=$m polybar --reload default &
+        elif [ $m == 'HDMI0' ]
+        then
+            MONITOR=$m polybar --reload default &
+        else
+            MONITOR=$m polybar --reload screen-2 &
+        fi     
+    done
 else
-  polybar --reload default &
+    polybar --reload default &
 fi
